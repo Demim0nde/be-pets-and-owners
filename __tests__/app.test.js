f913e1fe4ea9ea47cf176f659db2848427195626
Rose Mullan <rfmullan@gmail.com>
2025-02-13 13:52:15 +0000

    importing into test file

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,5 +1,7 @@
 // write your tests in here!
 const db = require("../db/connection")
+const seed = require("../db/seeding/seed")
+const data = require("../db/data/test-data")
 
 describe("Complete me...", () => { 
     test("Write your first test here...",() => { })
