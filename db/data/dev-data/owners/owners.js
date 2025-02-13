3192ccb3183164931f43f549484af13db1ec14de
Rose Mullan <rfmullan@gmail.com>
2025-02-13 11:50:37 +0000

    refactor data ready for databasing

---
+++ db/data/dev-data/owners/owners.js
@@ -0,0 +1,43 @@
+module.exports = [
+    {
+        name: "Shakira",
+        age: 48
+    },
+    {
+        name: "Barack",
+        age: 63
+    },
+    {
+        name: "Enrique",
+        age: 49
+    },
+    {
+        name: "Saoirse",
+        age: 30
+    },
+    {
+        name: "Hunter",
+        age: 26
+    },
+    {
+        name: "Cristiano",
+        age: 40
+    },
+    {
+        name: "Katya",
+        age: 412
+    },
+    {
+        name: "Cyndi",
+        age: 71
+    },
+    {
+        name: "Macaulay",
+        age: 10
+    },
+    {
+        name: "Sabrina",
+        age: 25
+    }
+
+]
---
+++ db/data/dev-data/pets/pets.js
@@ -0,0 +1,106 @@
+module.exports = [
+  {
+    name: "Greg Attenborough",
+    avatarUrl: "http://static.boredpanda.com/blog/wp-content/uuuploads/cute-baby-animals/cute-baby-animals-2.jpg",
+    favouriteFood: "Cucumber Sandwiches",
+    owner: "o1",
+    age: 12,
+    temperament: "passive"
+  },
+  {
+    name: "Chewie",
+    avatarUrl: "https://s-media-cache-ak0.pinimg.com/736x/b1/7d/49/b17d498b7e3559d5ba8c0de68e19e1b0.jpg",
+    favouriteFood: "Veal",
+    owner: "o3",
+    age: 13,
+    temperament: "happy"
+  },
+  {
+    name: "Porky Pines",
+    avatarUrl: "http://static.boredpanda.com/blog/wp-content/uuuploads/cute-baby-animals/cute-baby-animals-13.jpg",
+    favouriteFood: "Salad",
+    owner: "o2",
+    age: 9,
+    temperament: "grumpy"
+  },
+  {
+    name: "Alan Turin",
+    avatarUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjOqKI0kZG7nIV2w7AFRWfPUGiqeM0J26TbCp8irR1jZiNG556",
+    favouriteFood: "Digestive Biscuits",
+    owner: "o1",
+    age: 10,
+    temperament: "aggressive"
+  },
+  {
+    name: "Sebastian Smithers",
+    avatarUrl: "https://s-media-cache-ak0.pinimg.com/736x/63/bb/61/63bb616a33fc209cd3a48795c981f46d.jpg",
+    favouriteFood: "Jellied Eels",
+    owner: "o3",
+    age: 8,
+    temperament: "happy"
+  },
+  {
+    name: "Lady Penelope",
+    avatarUrl: "https://s-media-cache-ak0.pinimg.com/736x/4c/2a/15/4c2a150eaca5d299045818782e0d47a7.jpg",
+    favouriteFood: "Grapefruit",
+    owner: "o4",
+    age: 14,
+    temperament: "happy"
+  },
+  {
+    name: "Bill F Murray",
+    avatarUrl: "https://s-media-cache-ak0.pinimg.com/originals/82/6d/36/826d367313bfe7b57e26aa8f4de2d24e.jpg",
+    favouriteFood: "Pawpaw Fruit",
+    owner: "o4",
+    age: 15,
+    temperament: "grumpy"
+  },
+  {
+    name: "Waddles",
+    avatarUrl: "http://www.slate.com/content/dam/slate/blogs/wild_things/2015/09/02/150902_WILD_CutePenguins.jpg.CROP.promo-xlarge2.jpg",
+    favouriteFood: "Deep Fried Mars Bar",
+    owner: "o3",
+    age: 1,
+    temperament: "happy"
+  },
+  {
+    name: "Artemis",
+    avatarUrl: "https://s-media-cache-ak0.pinimg.com/736x/2a/e2/ef/2ae2ef93b2d3fc875d289883800b9e7c.jpg",
+    favouriteFood: "Marshmellows",
+    owner: "o3",
+    age: 2,
+    temperament: "grumpy"
+  },
+  {
+    name: "Dunston",
+    avatarUrl: "https://media.mnn.com/assets/images/2010/02/baby-orangutan.jpg.1000x0_q80_crop-smart.jpg",
+    favouriteFood: "Fish and Chips",
+    owner: "o1",
+    age: 4,
+    temperament: "aggressive"
+  },
+  {
+    name: "Mike Verternum",
+    avatarUrl: "http://lh5.ggpht.com/-t36nK4mKyeE/UpjQdOIcqtI/AAAAAAAAHUM/Q6VFu-Uvo9o/s1600/instaG_2013_11_29_15.01.34.jpg",
+    favouriteFood: "Bamboo",
+    owner: "o5",
+    age: 6,
+    temperament: "grumpy"
+  },
+  {
+    name: "Trash Panda",
+    avatarUrl: "https://img.buzzfeed.com/buzzfeed-static/static/2014-11/5/13/enhanced/webdr07/enhanced-5913-1415212088-2.jpg?downsize=715:*&output-format=auto&output-quality=auto",
+    favouriteFood: "KFC Gravy",
+    owner: "o4",
+    age: 5,
+    temperament: "passive"
+  },
+  {
+    name: "Timone",
+    avatarUrl: "https://s-media-cache-ak0.pinimg.com/736x/b8/23/89/b82389248a9b8a05294b68358790fe4a.jpg",
+    favouriteFood: "Bacon",
+    owner: "o2",
+    age: 11,
+    temperament: "aggressive"
+  }
+]
