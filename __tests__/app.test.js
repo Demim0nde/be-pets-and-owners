df2344bda760f4431667ea1f4299c09e894ffecc
Adam-Peel <122218248+Adam-Peel@users.noreply.github.com>
2025-06-03 12:02:50 +0100

    Write first test (API GET)

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,6 +4,7 @@ const seed = require("../db/seeding/seed");
 const data = require("../db/data/test-data");
 const request = require("supertest");
 const app = require("../app");
+// app.use(express.json());
 
 beforeEach(() => {
   return seed(data);
@@ -13,6 +14,16 @@ afterAll(() => {
   db.end();
 });
 
-describe("Complete me...", () => {
-  test("Write your first test here...", () => {});
+describe("GET API Testing", () => {
+  test("GET - 200: /api/owners/:id", () => {
+    return request(app)
+      .get("/api/owners/3")
+      .expect(200)
+      .then((body) => {
+        const { owner_id, name, age } = body;
+        expect(owner_id).toBe(3);
+        expect(typeof name).toBe("string");
+        expect(typeof age).toBe("number");
+      });
+  });
 });
