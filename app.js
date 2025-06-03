23b2a821ad658a3cc0abefa4ae2fa672c23ebe46
Demim0nde <tamnorth@proton.me>
2025-06-03 16:39:46 +0100

    fork branch for solo development

--- app.js
+++ app.js
@@ -21,33 +21,5 @@ app.get("/api/owners/:id", async (request, response) => {
   }
 });
 
-app.get("/api/owners", async (request, response) => {
-  try {
-    const { rows } = await db.query(`SELECT * FROM owners`);
-    response.status(200).send(rows);
-  } catch (err) {
-    response.status(500).send(err);
-    console.log(err);
-  }
-});
-
-app.get("/api/owners/:id/pets", async (request, response) => {
-  try {
-    const ownerId = Object.values(request.params);
-    const { rows } = await db.query(
-      `SELECT * FROM pets WHERE owner = $1`,
-      ownerId
-    );
-    if (rows.length > 0) {
-      response.status(200).send(rows);
-    } else {
-      response.status(404).send(rows);
-    }
-  } catch (err) {
-    response.status(500).send(err);
-    console.log(err);
-  }
-});
-
 // Listen
 module.exports = { app };
